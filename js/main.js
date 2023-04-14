6835613e7793adceb782d8f5b8d2c5b8a7d9d1c9
Nikita(Smail96251) <hveshchenko@gmail.com>
2023-04-14 17:55:28 +0300

    feat: add get number and sign

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+const buttons = document.querySelectorAll(".btn");
+const currentScreen = document.querySelector(".screen-current");
+const screenLast = document.querySelector(".screen-last");
 
+let firstNumber = "";
+let lastNumber = "";
+let sign = "";
+
+const numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+const signArray = ["÷", "x", "-", "+"];
+
+function getNumberAndSign(e) {
+  if (numberArray.includes(e.target.innerText)) {
+    if (sign === "" && lastNumber === "") {
+      firstNumber += e.target.innerText;
+      currentScreen.textContent = firstNumber;
+    } else {
+      lastNumber += e.target.innerText;
+      currentScreen.textContent = lastNumber;
+    }
+  } else {
+    if (signArray.includes(e.target.innerText)) {
+      sign = e.target.innerText;
+      screenLast.textContent = `${firstNumber} ${sign}`;
+    }
+  }
+}
+
+buttons.forEach((button) => button.addEventListener("click", getNumberAndSign));
